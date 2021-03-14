386765aeaf67738c70f6f31b2ef3b250e8f69e3d
htinlinnag1993@gmail.com <htinlinnag1993@gmail.com>
2021-03-13 21:23:44 -0500

    Add SideNavBar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import {BrowserRouter as Router, Route} from "react-router-dom";
-import Button from '@material-ui/core/Button';
+import {SideNavBar} from "./components/SideNavBar";
 import css from "./app.module.css";
 
 const App = () => (
-  <div>
-    <h1 className={css.appName}>My React and TypeScript App! {new Date().toLocaleDateString()}</h1>
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
-  </div>
+  <>
+    <SideNavBar />
+  </>
 );
 
 export {App};
